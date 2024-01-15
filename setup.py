a91fa8deef594dbc2154ee105ceacf6f16ef71a1
Benjamin James <benjames@mit.edu>
2024-01-15 17:44:31 -0500

    Use configure.ac to write eigen3, OpenMP flags with pkg-config

--- setup.py
+++ setup.py
@@ -1,23 +1,35 @@
 from setuptools import setup, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 import glob
+import os
 
 with open("VERSION", "r") as version_file:
         __version__ = version_file.read().strip()
 
+build_flags = {}        
+try:
+    with open("src/Makevars", "r") as makevars:
+        for line in makevars:
+            parts = line.strip().split("=")
+            if len(parts) == 2:
+                build_flags[parts[0]] = parts[1]
+except:
+    build_flags = {"OPENMP_CXXFLAGS": "-fopenmp",
+                   "OPENMP_LIBS": "-fopenmp",
+                   "EIGEN_CXXFLAGS": "",
+                   "EIGEN_LIBS": ""}
+    if os.getenv("CONDA_PREFIX"):
+        ### conda
+        build_flags["EIGEN_CXXFLAGS"] = "-I%s" % os.path.join(os.getenv("CONDA_PREFIX"), "include", "eigen3")
 ext_modules = [
    Pybind11Extension(
         "scdemon_ext",
            sources=sorted(glob.glob("scdemon/py*.cpp")),
-           include_dirs=["src",
-                         "/usr/include/eigen3",
-                         "/usr/local/include/eigen3",
-                         "../eigen",
-                         ],
+           include_dirs=["src"] + eigen_dirs,
            cxx_std=17,
            define_macros=[("VERSION_INFO", __version__)],
-           extra_compile_args=["-fopenmp"],
-           extra_link_args=["-fopenmp"]
+           extra_compile_args=[build_flags["OPENMP_CXXFLAGS"], build_flags["EIGEN_CXXFLAGS"]],
+           extra_link_args=[build_flags["OPENMP_LIBS"], build_flags["EIGEN_LIBS"]]
     ),
 ]
 
