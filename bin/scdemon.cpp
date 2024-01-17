ed7d0be922bb5e087bd696e0866f5a4f2bc42d76
Benjamin James <benjames@mit.edu>
2024-01-17 16:56:25 -0500

    Added cpp sample

---
+++ bin/scdemon.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+int main(int argc, char**argv)
+{
+  //const char* const short_opts = "i:u:v:o:";
+  // Eigen::MatrixXd X_pca = h5read(argv[1], "/obsm/X_pca");
+  // Eigen::VectorXd sigma = X_pca.colwise().norm();
+  // Eigen::MatrixXd PCs = h5read(argv[1], "/varm/PCs").transpose();
+  std::cout << argv[0] << std::endl;
+}
