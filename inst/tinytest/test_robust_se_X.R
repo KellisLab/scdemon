fb5fb154dd0f8a4c799411dce3802502c8aa0a85
Benjamin James <benjames@mit.edu>
2024-02-05 01:37:26 -0500

    Full coverage of robust_se_X in its test

--- inst/tinytest/test_robust_se_X.R
+++ inst/tinytest/test_robust_se_X.R
@@ -1,6 +1,7 @@
 set.seed(42)
 
 if (requireNamespace(c("lmtest", "sandwich"), quietly=TRUE)) {
+  ### For several matrices, make sure standard errors between columns are correct
   mat_nr <- c(100, 250, 500, 1000, 2000, 5000)
   mat_nc <- sample(seq(2, length(LETTERS)), length(mat_nr))
   apply(cbind(mat_nr, mat_nc), 1, function(shape) {
@@ -12,7 +13,16 @@ if (requireNamespace(c("lmtest", "sandwich"), quietly=TRUE)) {
       se <- lmtest::coeftest(model, vcov.=sandwich::vcovHC(model, "HC0"))
       return(se["x", "t value"])
     })
+    ### Make sure the matching is correct
+    for (cn in LETTERS) {
+      if (cn %in% colnames(mat)) {
+        expect_silent(scdemon::robust_se_X(cn, mat))
+      } else {
+        expect_error(scdemon::robust_se_X(cn, mat))
+      }
+    }
     predicted <- scdemon::robust_se_X(i, mat)
+    expect_equal(names(predicted), colnames(mat))
     expect_equal(actual, predicted[names(actual)])
   })
 }
